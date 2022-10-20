a029a21effccaf1c2e4e8b67cf0c50d3f7a8c0a4
Burhan Shaheen <burhan.shaheen@zaxiss.com>
2022-10-20 14:44:56 +0500

    simple project to create the custom clipper

---
+++ android/app/src/main/kotlin/com/example/flutter_customer_clipper/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_customer_clipper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Custom WaveClipper")),
+        body: Container(
+          child: Stack(children: [
+            Opacity(
+              opacity: 0.5,
+              child: ClipPath(
+                clipper: WaveClipper(),
+                child: Container(
+                  color: Colors.deepOrangeAccent,
+                  height: 200,
+                ),
+              ),
+            ),
+            ClipPath(
+              clipper: WaveClipper(),
+              child: Container(
+                color: Colors.red,
+                height: 180,
+                alignment: Alignment.center,
+                child: Text(
+                  'Here is wave clipper',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    debugPrint(size.width.toString());
+    var path = new Path();
+    path.lineTo(0, size.height);
+    var firstStart = Offset(size.width / 5, size.height);
+    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+    var secondEnd = Offset(size.width, size.height - 10.0);
+
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    // throw UnimplementedError();
+    return true;
+  }
+}
